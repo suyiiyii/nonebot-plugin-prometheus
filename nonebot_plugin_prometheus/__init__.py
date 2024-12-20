44acf8e6c5f6c07c48c066607096ef4f501c870f
suyiiyii <suyiiyii@gmail.com>
2024-12-20 21:54:36 +0800

    init

---
+++ nonebot_plugin_prometheus/__init__.py
@@ -0,0 +1,8 @@
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name="Prometheus 集成插件",
+    description="为 NoneBot 和其他插件提供 Prometheus 集成支持",
+    type="library",
+    usage="还不知道"
+)
