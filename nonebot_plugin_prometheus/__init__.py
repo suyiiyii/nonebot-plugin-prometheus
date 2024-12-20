86e303ee1f3e46e153aa6cc242df7e0d14e48368
suyiiyii <suyiiyii@gmail.com>
2024-12-20 22:21:21 +0800

    使用 nb 的抽象，定义 http endpoint

--- nonebot_plugin_prometheus/__init__.py
+++ nonebot_plugin_prometheus/__init__.py
@@ -1,3 +1,4 @@
+from nonebot.drivers.websockets import logger
 from nonebot.plugin import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
@@ -6,3 +7,25 @@ __plugin_meta__ = PluginMetadata(
     type="library",
     usage="还不知道"
 )
+
+from nonebot import get_driver
+from nonebot.drivers import URL, Request, Response, ASGIMixin, HTTPServerSetup
+
+
+async def hello(request: Request) -> Response:
+    return Response(200, content="Hello, world!")
+
+
+driver = get_driver()
+if not isinstance(driver, ASGIMixin):
+    logger.warning("Prometheus 插件未找到支持 ASGI 的驱动器")
+else:
+    logger.debug("找到支持 ASGI 的驱动器，Prometheus 插件开始加载")
+    driver.setup_http_server(
+        HTTPServerSetup(
+            path=URL("/metrics"),
+            method="GET",
+            name="hello",
+            handle_func=hello,
+        )
+    )
