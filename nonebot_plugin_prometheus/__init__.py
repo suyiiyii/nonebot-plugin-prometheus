34b9ee9aaa0c6fb1bf1308033f5ac2877896834d
suyiiyii <suyiiyii@gmail.com>
2024-12-20 23:06:46 +0800

    build(dev): 添加 Ruff 作为代码格式化和分析工具
    - 在项目中引入 Ruff 作为 dev 依赖项
    - 添加 .pre-commit-config.yaml 配置文件以支持 Ruff
    - 更新 pyproject.toml，将 nonebot2[fastapi] 从依赖移至 dev 依赖

--- nonebot_plugin_prometheus/__init__.py
+++ nonebot_plugin_prometheus/__init__.py
@@ -1,9 +1,9 @@
 from nonebot.plugin import PluginMetadata
-import nonebot_plugin_prometheus.api
+from nonebot_plugin_prometheus import api as api
+
 __plugin_meta__ = PluginMetadata(
     name="Prometheus 集成插件",
     description="为 NoneBot 和其他插件提供 Prometheus 集成支持",
     type="library",
-    usage="还不知道"
+    usage="还不知道",
 )
-
