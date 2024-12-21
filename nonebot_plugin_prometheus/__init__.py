96a4fea9e586893e4d663cf8655f37c7ea4fd1b9
suyiiyii <suyiiyii@gmail.com>
2024-12-21 10:21:47 +0800

    feat:定义 prometheus相关常量并更新导出列表
    - 导入 prometheus_client 模块
    - 定义 Counter、Gauge、Histogram 和 Summary 类
    - 更新 __all__ 列表，导出 prometheus_client 模块和相关类

--- nonebot_plugin_prometheus/__init__.py
+++ nonebot_plugin_prometheus/__init__.py
@@ -1,4 +1,12 @@
+import prometheus_client as prometheus_client
 from nonebot.plugin import PluginMetadata
+from prometheus_client import (
+    Counter as Counter,
+    Gauge as Gauge,
+    Histogram as Histogram,
+    Summary as Summary,
+)
+
 from nonebot_plugin_prometheus import api as api
 
 __plugin_meta__ = PluginMetadata(
@@ -7,3 +15,5 @@ __plugin_meta__ = PluginMetadata(
     type="library",
     usage="还不知道",
 )
+
+__all__ = [prometheus_client, Counter, Gauge, Histogram, Summary]
