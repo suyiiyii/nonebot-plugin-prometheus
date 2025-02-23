0421ee38614ef7acadf5a6a02bfcaffa8f2dacd2
Komorebi <110453675+KomoriDev@users.noreply.github.com>
2025-02-23 10:52:46 +0800

    refactor(counter): 使用 Alconna Extension 重写 Message Counter 部分 (#2)

--- nonebot_plugin_prometheus/__init__.py
+++ nonebot_plugin_prometheus/__init__.py
@@ -1,5 +1,5 @@
 import prometheus_client as prometheus_client
-from nonebot.plugin import PluginMetadata
+from nonebot.plugin import PluginMetadata, require
 from prometheus_client import (
     Counter as Counter,
     Gauge as Gauge,
@@ -7,8 +7,12 @@ from prometheus_client import (
     Summary as Summary,
 )
 
+require("nonebot_plugin_alconna")
+from nonebot_plugin_alconna import Command, add_global_extension
+
 from nonebot_plugin_prometheus import api as api
 from nonebot_plugin_prometheus.config import Config
+from nonebot_plugin_prometheus.extension import MessageCounterExtension
 
 __plugin_meta__ = PluginMetadata(
     name="Prometheus 监控",
@@ -21,3 +25,14 @@ __plugin_meta__ = PluginMetadata(
 )
 
 __all__ = [prometheus_client, Counter, Gauge, Histogram, Summary]
+
+add_global_extension(MessageCounterExtension)
+
+metrics = (
+    Command("metrics", help_text="查询指标数据")
+    .usage(__plugin_meta__.usage)
+    .build(
+        block=True,
+        use_cmd_start=True,
+    )
+)
